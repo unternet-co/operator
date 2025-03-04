663793d8103aae6bd19f4c9c55d6163620cc35d9
Rupert <mail@ruperts.world>
2025-03-04 09:13:45 -0800

    Removed dead code

--- kernel/lib/model.ts
+++ kernel/lib/model.ts
@@ -1,30 +1,22 @@
-import OpenAI from 'openai/index.mjs';
 import { createOpenAI } from '@ai-sdk/openai';
 import { createOllama } from 'ollama-ai-provider';
-import { generateObject,
-  CoreSystemMessage,
-  CoreUserMessage,
-  CoreAssistantMessage,
-  CoreToolMessage
-} from 'ai';
 
-export const DEFAULT_MODEL_OLLAMA = 'qwen2.5-coder:3b'
-export const DEFAULT_MODEL_OPENAI = 'gpt-4o'
-
+export const DEFAULT_MODEL_OLLAMA = 'qwen2.5-coder:3b';
+export const DEFAULT_MODEL_OPENAI = 'gpt-4o';
 
 export interface modelOptions {
-  type?: string,
-  model?: string,
-  apiKey?: string,
-  baseURL?: string
+  type?: string;
+  model?: string;
+  apiKey?: string;
+  baseURL?: string;
 }
 
 export function fromConfig({
-    type='openai',
-    model,
-    apiKey=import.meta.env.VITE_OPENAI_API_KEY,
-    baseURL
-  }:modelOptions = {}) {
+  type = 'openai',
+  model,
+  apiKey = import.meta.env.VITE_OPENAI_API_KEY,
+  baseURL,
+}: modelOptions = {}) {
   if (type === 'ollama' || !apiKey) {
     return createOllama({
       baseURL,
@@ -36,31 +28,4 @@ export function fromConfig({
       compatibility: 'strict',
     })(model || DEFAULT_MODEL_OPENAI);
   }
-}
-
-export const openai = createOpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  compatibility: 'strict',
-});
-
-export const model = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
-
-interface GenerateJsonParams {
-  prompt?: string;
-  messages?: Array<CoreSystemMessage | CoreUserMessage | CoreAssistantMessage | CoreToolMessage>;
-  schema: any;
-}
-export async function generateJson({ messages, schema }: GenerateJsonParams, options?: modelOptions) {
-  const model = fromConfig(options)
-
-  const { object } = await generateObject({
-    model,
-    messages,
-    schema
-  })
-
-  return {json: object}
 }
